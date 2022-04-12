d104b71b5f09e4d4444adaafcc7553d280c410fe
何权利 <hequanli@tinman.cn>
2022-04-12 17:41:17 +0800

    feat: 添加公共loadingDialog

--- module/module_base/build.gradle.kts
+++ module/module_base/build.gradle.kts
@@ -1,6 +1,6 @@
 dependencies {
     implementation(project(mapOf("path" to ":core:core_http")))
-
+    implementation(project(mapOf("path" to ":core:core_sw")))
     implementation(libs.coreKtx)
     implementation(libs.appcompat)
     implementation(libs.material)
---
+++ module/module_base/src/main/java/com/hql/spark/module/base/widget/loading/AnimatorListener.java
@@ -0,0 +1,22 @@
+package com.hql.spark.module.base.widget.loading;
+
+import android.animation.Animator;
+
+
+public abstract class AnimatorListener implements Animator.AnimatorListener {
+
+    @Override
+    public void onAnimationStart(Animator animation) {
+
+    }
+
+    @Override
+    public void onAnimationCancel(Animator animation) {
+
+    }
+
+    @Override
+    public void onAnimationRepeat(Animator animation) {
+
+    }
+}
